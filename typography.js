12b54f70e7dfce7e87ad57c70eb64993615550c9
Matt Thorning <matthewthorning@gmail.com>
2019-01-25 20:40:15 +0000

    Fix typo and change font

--- typography.js
+++ typography.js
@@ -1,6 +1,7 @@
 import Typography from "typography"
 import sternGrove from "typography-theme-stern-grove"
 
+sternGrove.bodyFontFamily = ['Quicksand', 'sans-serif']
 const typography = new Typography(sternGrove)
 
 export default typography
