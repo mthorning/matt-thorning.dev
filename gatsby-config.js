8ac43a0351bbc552152f3fbf7ef21f1c7f26e825
Matt Thorning <matthewthorning@gmail.com>
2019-01-30 21:47:39 +0000

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              imagesPath: '!*.gif',
+              imagesPath: '*.gif',
               linksPath: '*',
             },
           },
