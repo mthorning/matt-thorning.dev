d37e7cb2bc153939de1175c5fed53d0be9b5b97c
Matt Thorning <matthewthorning@gmail.com>
2019-03-02 15:45:15 +0000

    duplicated code in gatsby-browser and gatsby-ssr in attempted bug fix for missing css
    (cherry picked from commit c2286205b2136de7ffac1f9cff7c4dd3ccb294a2)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,15 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
+import { ThemeProvider } from 'emotion-theming'
+import { Global } from '@emotion/core'
+import { theme, global } from './src/styles'
+import('prismjs/themes/prism-tomorrow.css')
+import('prismjs/plugins/line-numbers/prism-line-numbers.css')
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      {element}
+    </ThemeProvider>
+  )
+}
