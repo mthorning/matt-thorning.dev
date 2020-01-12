23ed784b3e0373d04eb291850f305aabcc50e44b
Matt Thorning <matthewthorning@gmail.com>
2020-01-12 11:13:23 +0000

    added RustOutput component

---
+++ src/mdx-components/RustOutput.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+const style = css`
+  display: block;
+  overflow-x: auto;
+  background: #f1f1f1;
+  color: #6e6b5e;
+  padding: 0.5em;
+`
+const errorStyle = css`
+  outline: 0.5em solid #e8111294;
+  padding: 0;
+`
+export default function RustOutput({ output, error }) {
+  const styles = [style]
+  if (error) styles.push(errorStyle)
+  return <pre css={styles}>{output}</pre>
+}
