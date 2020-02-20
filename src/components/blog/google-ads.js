43356f35faa461ab67e5b76afda17494855e45de
Matt Thorning <matthewthorning@gmail.com>
2020-02-20 22:37:07 +0000

    fix failing build

--- src/components/blog/google-ads.js
+++ src/components/blog/google-ads.js
@@ -17,7 +17,7 @@ export function FooterAdd() {
   return (
     <ins
       class="adsbygoogle"
-      style="display:block; text-align:center;"
+      style={{ display: 'block', textAlign: 'center' }}
       data-ad-layout="in-article"
       data-ad-format="fluid"
       data-ad-client="ca-pub-9458112330628025"
